0e6dadf160b6773be3a4d34f8901694f3ac3a3b5
rachOS <g.chamekh@e-nautia.com>
2023-03-13 22:02:01 +0100

    refactor(Options): clean code

--- src/components/organisms/Options.tsx
+++ src/components/organisms/Options.tsx
@@ -7,38 +7,43 @@ import React, {
   useState,
 } from "react";
 import ScoopOptions from "../organisms/ScoopOptions";
-import {useAxios} from "hooks/useAxios";
+import { useAxios } from "hooks/useAxios";
 import ToppingOptions from "../organisms/ToppingOptions";
 import "./style.module.css";
-import {useOrderDetailsContext} from "../../contexts/ordersContext";
+import { useOrderDetailsContext } from "../../contexts/ordersContext";
 
 type Props = {
   optionsType: string;
 };
 
-const Options: FC<Props> = ({optionsType = "scoops"}: Props): JSX.Element => {
+const formatCurrencyHelper = (currency: number | bigint) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(currency);
+};
+
+const Options: FC<Props> = ({ optionsType = "scoops" }: Props): JSX.Element => {
   const [data, error] = useAxios<IMG>(`http://localhost:5173/${optionsType}`);
-  const [scoops, setScoops] = useState<Map<string, number>>(new Map());
-  const [toppingsTotal, setToppingsTotal] = useState<string>("0.00");
+  const [scoops, setScoops] = useState<Map<string, number>>(
+    new Map()
+  ); /*TODO useReducer for scoops / toppings */
   const [form, setForm] = useState<{ [key: string]: string }>({});
 
-  const {prices, handleSetTotal} = useOrderDetailsContext();
+  const { prices, handleSetTotal } = useOrderDetailsContext();
 
   const isScoops = optionsType === "scoops";
 
   const handleChange = useCallback(
-      ({target: {name, value}}: ChangeEvent<HTMLInputElement>) => {
-        setForm((prevForm) => ({...prevForm, [name]: value}));
+    ({ target: { name, value } }: ChangeEvent<HTMLInputElement>) => {
+      setForm((prevForm) => ({ ...prevForm, [name]: value }));
 
-        if (isScoops) {
-          setScoops((prevScoops) => prevScoops.set(name, Number(value)));
-        }
-
-        setToppingsTotal((prevState) =>
-            (Number(prevState) + Number(value)).toString()
-        );
-      },
-      [isScoops, prices]
+      if (isScoops) {
+        setScoops((prevScoops) => prevScoops.set(name, Number(value)));
+      }
+    },
+    [isScoops, prices]
   );
 
   const ItemOptions = isScoops ? ScoopOptions : ToppingOptions;
@@ -52,37 +57,29 @@ const Options: FC<Props> = ({optionsType = "scoops"}: Props): JSX.Element => {
     return tmp.reduce((a, b) => a + b, 0) * prices[optionsType];
   }, [prices, optionsType, scoops]);
 
-  const formatCurrencyHelper = (currency: number | bigint) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(currency);
-  };
-
   useEffect(
-      () => handleSetTotal("scoops", formatCurrencyHelper(calcScoopTotal())),
-      [calcScoopTotal, form]
+    () => handleSetTotal("scoops", formatCurrencyHelper(calcScoopTotal())),
+    [calcScoopTotal, form]
   );
 
   return (
-      <div>
-        {error ? (
-            <div role={"alert"}>{error.message}</div>
-        ) : (
-            data?.map(
-                ({name, path}: IMG): JSX.Element => (
-                    <ItemOptions
-                        key={name}
-                        name={name}
-                        path={path}
-                        handleChange={handleChange}
-                        value={form[name]}
-                    />
-                )
-            )
-        )}
-      </div>
+    <div>
+      {error ? (
+        <div role={"alert"}>{error.message}</div>
+      ) : (
+        data?.map(
+          ({ name, path }: IMG): JSX.Element => (
+            <ItemOptions
+              key={name}
+              name={name}
+              path={path}
+              handleChange={handleChange}
+              value={form[name]}
+            />
+          )
+        )
+      )}
+    </div>
   );
 };
 
